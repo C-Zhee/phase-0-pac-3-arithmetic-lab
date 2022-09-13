2b279918abde4bafae730df2837ea56c4db655ee
Corey <coreyzhee@gmail.com>
2022-09-13 17:11:51 -0400

    commit tree 93506ab36f5fcea77c7e6188eede2eab6076e47b

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b) {
+    return a+b
+}
+console.log(add(400,452))
+
+function subtract(a,b) {
+    return a-b
+}
+console.log(subtract(1,2))
+
+function multiply(a,b) {
+    return a*b
+}
+console.log(multiply(1,2))
+
+function divide(a,b) {
+    return a/b
+}
+console.log(divide(1,2))
+
+function increment(n) {
+    return n+=1
+}
+console.log(increment(300))
+
+function decrement(n) {
+    return n-=1
+}
+console.log(increment(20));
+
+function makeInt(string) {
+    return parseInt (string,10)
+}
+
+function preserveDecimal(string){
+    return parseFloat(string)
+}
+
